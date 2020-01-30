690541b9e47f13bc2a0632e2f960364331af340f
Daniel Fieker <danielmfieker@gmail.com>
2020-01-30 15:39:46 -0600

    Finishing App.js and Form.js

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -6,25 +6,14 @@ function App() {
   const [list, setList] = useState([
     {
       Name: 'Daniel',
-      Email: 'daniel@yahoo.com',
+      Email: 'daniel1972846293847102937@yoohoo.com',
       Role: 'WebDev'
     }
   ])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
